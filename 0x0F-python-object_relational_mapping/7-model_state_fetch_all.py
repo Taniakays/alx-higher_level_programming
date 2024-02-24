03fc912b2809cdbe5cad3dcf63d485dd2a564b17
Taniakays <ms.tkatiyo@gmail.com>
2024-02-24 05:02:54 -0800

    object relational

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
+"""Start link class to table in database
 """
-Contains State class and Base, an instance of declarative_base()
-"""
-from sqlalchemy import Column, Integer, String, MetaData
-from sqlalchemy.ext.declarative import declarative_base
-
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 
-class State(Base):
-        """
-            Class with id and name attributes of each state
-                """
-                    __tablename__ = 'states'
-                        id = Column(Integer, unique=True, nullable=False, primary_key=True)
-                            name = Column(String(128), nullable=False)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,7 +2,7 @@
 """
 Contains the class definition of a City
 """
-from relationship_state import Base
+from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
